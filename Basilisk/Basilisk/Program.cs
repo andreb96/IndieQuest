3122865483d94c2612e873e9d28341f77a6d99b5
andreb96 <71494245+andreb96@users.noreply.github.com>
2020-10-04 20:45:07 +0200

    Completed

---
+++ Basilisk/Basilisk/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basilisk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NameList = new List<string> { "Seofl", "André", "Css", "Omar" };
+            String Names = "";
+            var numbergen = new Random();
+            int basiliskHp = 0;
+            foreach (string Name in NameList)
+            {
+                Names += Name + " ";
+            }
+            Console.WriteLine($"A party of warriors ({Names}) descends into the dungeons");
+            for (int i = 0; i < 8; i++)
+            {
+                basiliskHp += numbergen.Next(1, 9);
+            }
+            basiliskHp += 16;
+            Console.WriteLine($"A basilisk with {basiliskHp}HP appears!");
+            while (basiliskHp > 0) {
+                if (NameList.Count == 0)
+                {
+                    break;
+                }
+                foreach (string Name in NameList)
+                { 
+                    int damage = 0;
+                    damage += numbergen.Next(1, 5);
+                    basiliskHp -= damage;
+                    if (basiliskHp <= 0)
+                    {
+                        Console.WriteLine($"{Name} hits the basilisk for {damage}. Basilisk has died.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{Name} hits the basilisk for {damage}. Basilisk has {basiliskHp}HP left.");   
+                    }
+                }
+                if (basiliskHp > 0) { 
+                
+                var BasiliskTargetIndex = numbergen.Next(0, NameList.Count);
+                var BasiliskTarget = NameList[BasiliskTargetIndex];
+                Console.WriteLine($"The Basilisk uses petrifying gaze on {BasiliskTarget}");
+                int ConstitutionRoll = numbergen.Next(1, 21);
+                int Constitution = 5 + ConstitutionRoll;
+                if (Constitution < 12)
+                {
+                    Console.WriteLine($"{BasiliskTarget} rolls {ConstitutionRoll} and is petrified.");
+                    NameList.Remove(BasiliskTarget);
+                }
+                else
+                {
+                    Console.WriteLine($"{BasiliskTarget} rolls {ConstitutionRoll} and is saved.");
+                }
+                }
+                
+            }
+            if (basiliskHp <= 0)
+            {
+                Console.WriteLine("The basilisk collapses and the heroes celebrate their victory!");
+            }
+            else
+            {
+                Console.WriteLine("The heroes has all been petrified, the basilisk wins.");
+            }
+
+
+        }
+    }
+}
---
+++ Tank/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Win32.SafeHandles;
+
+namespace Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name, commander?");
+            String Name = Console.ReadLine();
+            Random DistanceOfTAnk = new Random();
+            int TankDistance = DistanceOfTAnk.Next(40, 71);
+            String result = "_/";
+            Console.WriteLine($"Commander {Name}, the enemy is sending a tank towards us. We have to use our artilery to shoot it down");
+            for (int i = 0; i < 78; i++){
+                
+                if (i != TankDistance)
+                {
+                    result += "_";
+                }
+                else
+                {
+                    result += "T";
+                }
+                
+            }
+            Console.WriteLine(result);
+            int NumberOfShots = 5;
+            bool TankHit = false;
+            while (NumberOfShots > 0) {
+                Console.WriteLine($"Aim your shot, {Name}");
+                String numberText = Console.ReadLine();
+                int ShotDistance = Int32.Parse(numberText);
+                String BlastRange = "";
+                for (int i = 0; i <= ShotDistance + 2; i++)
+                {
+
+                    if (i != ShotDistance + 2)
+                    {
+                        BlastRange += " ";
+                    }
+                    else
+                    {
+                        BlastRange += "*";
+                    }
+
+                }
+                Console.WriteLine(BlastRange);
+                if (ShotDistance < TankDistance)
+                {
+                    Console.WriteLine("Oh no your shot was to short.");
+                }
+                else if(ShotDistance > TankDistance)
+                {
+                    Console.WriteLine("Oh no your shot was to far.");
+                }
+                else
+                {
+                    TankHit = true;
+                    Console.WriteLine($"The tank has been destroyed, nice shot {Name}"); 
+                    break;
+                }
+                NumberOfShots--;
+
+            }
+            if (!TankHit)
+            {
+                Console.WriteLine("You are out of ammunition, game over");
+            }
+
+
+        }
+    }
+}
